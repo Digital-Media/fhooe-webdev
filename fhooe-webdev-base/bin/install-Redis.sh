f97436a21e3d230f7be14977d2d18c448f2fbf9a
Wolfgang Hochleitner <wolfgang@hochleitner.cc>
2021-02-08 17:37:07 +0100

    Added scripts for installing ElasticSearch, PostgreSQL and Redis.
    Removed old start and stop scripts for ElasticSearch.

---
+++ fhooe-webdev-base/bin/install-Redis.sh
@@ -0,0 +1,30 @@
+# install Redis Server
+cd $HOME
+wget http://download.redis.io/redis-stable.tar.gz
+tar xzf redis-stable.tar.gz
+sudo apt-get install tcl
+cd redis-stable
+make
+make test
+sudo bash -c "echo 'cd $HOME/redis-stable' > /usr/local/bin/StartRedis.sh"
+sudo bash -c "echo 'nohup ./src/redis-server ./redis.conf &>redis.out &' >> /usr/local/bin/StartRedis.sh"
+sudo bash -c "echo 'sudo netstat -nlp|grep redis' >> /usr/local/bin/StartRedis.sh"
+sudo bash -c "echo 'cd $HOME/redis-stable'  > /usr/local/bin/StopRedis.sh"
+var='pid=`ps -ef|grep redis|head -1|awk '
+var+="'"
+var+='{print $2}'
+var+="'"
+var+='`'
+sudo chmod o+w /usr/local/bin/StopRedis.sh
+echo $var >> /usr/local/bin/StopRedis.sh
+sudo chmod o-w /usr/local/bin/StopRedis.sh
+sudo bash -c "echo 'kill -15 \$pid' >> /usr/local/bin/StopRedis.sh"
+sudo bash -c "echo 'sleep 10' >> /usr/local/bin/StopRedis.sh"
+sudo bash -c "echo 'sudo netstat -nlp|grep redis' >> /usr/local/bin/StopRedis.sh"
+sudo bash -c "echo 'cd $HOME/redis-stable'  >> /usr/local/bin/StopRedis.sh"
+sudo bash -c "echo 'rm redis.out dump.rdb' >> /usr/local/bin/StopRedis.sh"
+sudo chmod a+x /usr/local/bin/*Redis.sh
+cd $HOME
+rm redis-stable.tar.gz
+sed -i 's/# requirepass foobared/requirepass geheim/g' $HOME/redis-stable/redis.conf
+sudo apt-get install php-redis
